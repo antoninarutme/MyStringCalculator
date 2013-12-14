54172800b08d5714ef4afb6ae61238ba5f1b7d19
antoninarutme <hanantonina@gmail.com>
2013-12-14 14:29:24 +0200

    add shouldReturnZeroWhenEmptyString() test RED

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+package stringcalculator;
+
+public class Calculator {
+	
+	//helps
+    public static final String DEFAULT_DELIMITER_REGEX = "[,\n]";
+    public static final String PREFIX = "//";
+
+    public int add(String s)
+    {
+    	int sum = 0;
+    	if (s.isEmpty()) return 0;
+    	return sum;
+    }
+
+}
---
+++ TestCalculator.java
@@ -0,0 +1,21 @@
+package stringcalculator;
+
+import static junit.framework.Assert.assertEquals;
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalculator {
+	
+	private final Calculator calculator = new Calculator();
+
+    @SuppressWarnings("deprecation")
+	@Test
+    public void shouldReturnZeroWhenEmptyString()  {
+    	//act//
+    	int act = calculator.add("");
+    	//assert
+    	assertEquals(1, act);
+    }
+
+}
